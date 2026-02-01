d7a65220affe188090048cd896acfda7954dbc12
Nataliia Prokopchuk <74053094+Nataliia94@users.noreply.github.com>
2026-02-01 16:55:48 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+/*
+    Author: Code Jungle
+    Youtube: https://www.youtube.com/@CodeJungle
+    Instagram: https://www.instagram.com/code.jungle/
+*/
+
+// window scroll
+$(window).on("scroll", function() {
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= 100){
+        $('body').addClass('fixed-header');
+    } else {
+        $('body').removeClass('fixed-header')
+    }
+});
+
+// Document Ready
+$(document).ready(function() {
+
+    // Typing animation
+    new Typed('#type-it', {
+        strings: ['<span style="color:#87CEEB;">Designer</span>', 
+                  '<span style="color:#F17274;">Developer</span>', 
+                  '<span style="color:#DB58A0;">Freelancer</span>'],
+        typeSpeed: 100,
+        loop: true
+      });
+
+    // Owl Carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 2,
+        margin:30,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            900:{
+                items:2
+            },
+        }
+    });
+
+    // One Page Scroll
+    $.scrollIt({
+        easing: 'linear',      // the easing function for animation
+        topOffset: -70           // offste (in px) for fixed top navigation
+      });
+});
